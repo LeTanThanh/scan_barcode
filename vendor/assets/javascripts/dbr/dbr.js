49156c8ab3c10d626e377ea7eb1937230ee6a464
LeTanThanh <tanthanhcr95@gmail.com>
2018-12-15 16:00:22 +0700

    comment_dbr_html

--- vendor/assets/javascripts/dbr/dbr.js
+++ vendor/assets/javascripts/dbr/dbr.js
@@ -1,16 +1,10 @@
-var bPC = !navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i);
 var bMobileSafari = /Safari/.test(navigator.userAgent) && /iPhone/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent);
 
-if(bPC) {
-  // use upper resolution & read full region on PC
-  $('#ul-resolutionList .selectedLi').removeClass('selectedLi');
-  $('#ul-resolutionList .li-resolution[data-width="1920"][data-height="1080"]').addClass('selectedLi');
-  $('#lb-readFullRegion').click();
-}
+var preSelBarcodeFormat = 0;
+preSelBarcodeFormat = preSelBarcodeFormat | "0x3ff";
 
-$('#ul-supportBrowser').foundation();
-$('#ul-menu').foundation();
-$('#frame-white').hide();
+var isLooping = false;
+var readInterval = 10;
 
 dynamsoft.dbrEnv.resourcesPath = 'https://demo.dynamsoft.com/dbr_wasm/js';
 dynamsoft.dbrEnv.licenseKey = "t0068MgAAAKHUBCtgPRN4K2K4LGwxT8X0R54NyMhvqnpNkwguvI5gKi4r0kai6yo5q+PLIMV7a0+FaQUd1zLW8G5Yku/Twc0=";
@@ -30,54 +24,13 @@ dynamsoft.dbrEnv.onAutoLoadWasmSuccess = function() {
   }
 
   $('#div-highLightResult span').text('reading...');
-  $('#frame-loadingAnimation').hide();
-  $('.disableOnWasmLoading').removeClass('disableOnWasmLoading');
 };
 
 dynamsoft.dbrEnv.onAutoLoadWasmError = function(ex) {
   alert("load wasm failed: "+ (ex.message || ex));
 };
 
-$('#frame-supportAndSetting input').change(function() {
-  /*Start: walkaround for mobile safari ui delay bug*/
-  if(this.checked) {
-    $(this).closest('.div-format-switch').css('background','#2ba6cb');
-  } else {
-    $(this).closest('.div-format-switch').css('background','');
-  }
-  /*End: walkaround for mobile safari ui delay bug*/
-
-  if($('#frame-supportAndSetting input:checked').length) {
-    $('#btn-processSupportAndSettings').removeAttr('disabled');
-  } else {
-    $('#btn-processSupportAndSettings').attr('disabled', 'disabled');
-  }
-});
-
-var preSelBarcodeFormat = 0;
-$('#btn-processSupportAndSettings').click(function() {
-  $('#frame-supportAndSetting').fadeOut();
-
-  var checkedIpts = $('#frame-supportAndSetting input:checked');
-  checkedIpts.each(function() {
-    preSelBarcodeFormat = preSelBarcodeFormat | this.value;
-  });
-
-  if(bDbrWasmLoadSuccess) {
-    initTestRuntimeSettings().then(function() {
-      self.isLooping = true;
-      playvideo().then(loopReadVideo, function(ex) {
-        alert("Please make sure the camera is connected and the site is deployed in https: " + (ex.message || ex));
-      });
-    });
-  }
-});
-
 var $ulVideoList = $('#ul-videoList');
-
-var bReadFullRegion = false;
-
-var readInterval = 10;
 
 var testRuntimeSettingsReader = null;
 var initTestRuntimeSettings = function() {
@@ -86,8 +39,6 @@ var initTestRuntimeSettings = function() {
   settings.mBarcodeFormatIds = preSelBarcodeFormat;
   return testRuntimeSettingsReader.updateRuntimeSettings(settings);
 };
-
-var needFixEdge = false;
 
 var playvideo = function(deviceId) {
   return new Promise(function(resolve,reject) {
@@ -99,8 +50,8 @@ var playvideo = function(deviceId) {
       });
     }
 
-    var selW = $('#ul-resolutionList .selectedLi').attr('data-width');
-    var selH = $('#ul-resolutionList .selectedLi').attr('data-height');
+    var selW = 1280;
+    var selH = 720;
     var constraints = {
       video: {
         facingMode: { ideal: 'environment' }
@@ -139,18 +90,6 @@ var playvideo = function(deviceId) {
           video.srcObject = stream;
           video.onloadedmetadata = function() {
             video.play().then(function() {
-              if(needFixEdge) {
-                var dw = $(document).width(), dh = $(document).height();
-
-                if(video.videoWidth / video.videoHeight > dw / dh){
-                  video.style.width = Math.round(video.videoWidth / video.videoHeight * dh) + 'px';
-                  video.style.height = dh + 'px';
-                } else {
-                  video.style.width = dw + 'px';
-                  video.style.height = Math.round(video.videoHeight / video.videoWidth * dw) + 'px';
-                }
-              }
-
               resolve2();
             },function(ex) {
               reject2(ex);
@@ -176,7 +115,6 @@ var playvideo = function(deviceId) {
   });
 };
 
-var isLooping = false;
 var loopReadVideo = function() {
   if(!isLooping) {
     return;
@@ -191,27 +129,21 @@ var loopReadVideo = function() {
   var videoRealH = video.videoHeight;
   var sx, sy, sWidth, sHeight, dWidth, dHeight;
 
-  if(bReadFullRegion) {
-    sx = sy = 0;
-    sWidth = dWidth = videoRealW;
-    sHeight = dHeight = videoRealH;
+  var vw = $(document).width();
+  var vh = $(document).height();
+  var videoInDocRealW, videoInDocRealH;
+  if(vw / vh < videoRealW / videoRealH) {
+    videoInDocRealW = vw / vh * videoRealH;
+    videoInDocRealH = videoRealH;
   } else {
-    var vw = $(document).width();
-    var vh = $(document).height();
-    var videoInDocRealW, videoInDocRealH;
-    if(vw / vh < videoRealW / videoRealH) {
-      videoInDocRealW = vw / vh * videoRealH;
-      videoInDocRealH = videoRealH;
-    } else {
-      videoInDocRealW = videoRealW;
-      videoInDocRealH = vh / vw * videoRealW;
-    }
-
-    var regionRealWH = Math.round(/*0.6 * */Math.min(videoInDocRealW, videoInDocRealH));//looks like 0.6, real is 1
-    sx = Math.round((videoRealW - regionRealWH)/2);
-    sy = Math.round((videoRealH - regionRealWH)/2);
-    sWidth = sHeight = dWidth = dHeight = regionRealWH;
+    videoInDocRealW = videoRealW;
+    videoInDocRealH = vh / vw * videoRealW;
   }
+
+  var regionRealWH = Math.round(/*0.6 * */Math.min(videoInDocRealW, videoInDocRealH));//looks like 0.6, real is 1
+  sx = Math.round((videoRealW - regionRealWH)/2);
+  sy = Math.round((videoRealH - regionRealWH)/2);
+  sWidth = sHeight = dWidth = dHeight = regionRealWH;
 
   var timestart = null;
   var barcodeReader = new dynamsoft.BarcodeReader();
@@ -231,21 +163,6 @@ var loopReadVideo = function() {
           bestConfidence = confidence;
           bestTxt = result.BarcodeText;
         }
-      }
-    }
-
-    //add to top log
-    var $divTopLog = $('#div-topLog');
-    for(var i = 0; i < txtArr.length; ++i) {
-      var pTopLog = document.createElement("p");
-      pTopLog.style.display = 'none';
-      pTopLog.innerText = txtArr[i];
-      $divTopLog.append(pTopLog);
-      $(pTopLog).slideDown();
-      var pTopLogs = $divTopLog.children();
-
-      if(pTopLogs.length > $divTopLog.height() / $(pTopLogs[0]).outerHeight(true)) {
-        $(pTopLogs[0]).remove();
       }
     }
 
