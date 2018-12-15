5ebb8e1532034ef3002b18d5b2ea300480f1d8b8
LeTanThanh <tanthanhcr95@gmail.com>
2018-12-15 17:44:24 +0700

    add_scan_index_page

--- app/controllers/scan_controller.rb
+++ app/controllers/scan_controller.rb
@@ -1,4 +1,7 @@
 class ScanController < ApplicationController
+  def index
+  end
+
   def dbr
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root "scan#dbr"
+  root "scan#index"
+
+  get "/dbr", to: "scan#dbr", as: :dbr
 end
